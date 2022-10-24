4d70168e597874bcb923a7327bed9121092b7380
HannachiHassen <hannachihassen07@gmail.com>
2022-10-24 14:09:12 -0400

    Update java class:'FindLargestNumberInArray'

--- src/com/interviewQuestions/FindLargestNumberInArray.java
+++ src/com/interviewQuestions/FindLargestNumberInArray.java
@@ -11,17 +11,26 @@ package com.interviewQuestions;
  */
 public class FindLargestNumberInArray {
 
-	static int array []= {21,98,13,9,34};
+	static int [] array= {21,98,13,9,34};
 	
 	public static void main(String[] args) {
 		int maxNumber=findLargestNumber();
 		System.out.println(""+maxNumber);
 	}
 	
+	/**
+	 * 
+	 * @author Hassen
+	 * Oct 24, 2022
+	 * @return integer max
+	 */
 	public static int findLargestNumber() {
 		int max=array[0];
-		for() {
-			
-		}		
+		for(int i=0; i<array.length; i++) {
+			if (array[i]>max){
+				max=array[i];
+			}
+		}
+		return max;		
 	}
 }
