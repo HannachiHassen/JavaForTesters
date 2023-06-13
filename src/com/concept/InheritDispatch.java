bf47109f0cbe9157f87b40a9536606f31f3519db
HannachiHassen <hannachihassen07@gmail.com>
2023-06-13 15:14:11 -0400

    update InheritDispatch java class

--- src/com/concept/InheritDispatch.java
+++ src/com/concept/InheritDispatch.java
@@ -1,26 +1,37 @@
 package com.concept;
 
-public final class InhertitDispatch {
+public final class InheritDispatch {
 	
 	public static void main(String[] args) {
-		
+		AA a=new AA();
+		BB b=new BB();
+		C c=new C();
+		AA r;
+		r=a;
+		r.callme();
+		r=b;
+		r.callme();
+		r=c;
+		r.callme();
 	}
-	
-	class A{
+}
+	class AA{
 		void callme(){
 		System.out.println("Inside A's callme method");
 		}
 	}
 
-	class BB extends A{
+	class BB extends AA{
 		void callme(){
 		System.out.println("Inside B's callme method");
 		}
 	}
 	
-	class c extends A{
-		
+	class C extends AA{
+		void callme(){
+			System.out.println("Inside C's callme methos");
+			}
 	}
 	
-}
+
 
